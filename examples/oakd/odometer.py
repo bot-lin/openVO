a79e430bea1b3a3eb7845f437a02b7ffa58abe6b
bot-lin <zlin6364@hotmail.com>
2023-05-16 12:54:04 +0800

    Update odometer.py

--- examples/oakd/odometer.py
+++ examples/oakd/odometer.py
@@ -18,10 +18,10 @@ while True:
     rgb_frame = cv2.resize(rgb_frame, (640, 480))
     pose = odom.current_pose()
     print(pose)
-    drawPoseOnImage(pose, rgb_frame)
-    cv2.imshow("Annotated", rgb_frame)
-    # quit if the user presses q
-    if cv2.waitKey(100) & 0xFF == ord("q"):
-        break
+    # drawPoseOnImage(pose, rgb_frame)
+    # cv2.imshow("Annotated", rgb_frame)
+    # # quit if the user presses q
+    # if cv2.waitKey(100) & 0xFF == ord("q"):
+    #     break
 
 cam.stop()
